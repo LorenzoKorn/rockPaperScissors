270b607bba9381dd5297b4e5c7193f8c3d39e332
lorenzo korn <lkorn.9520@gmail.com>
2019-10-01 16:09:21 +0200

    initialized recyclerView

--- app/src/main/java/com/example/rockpapersciccors/controllers/History.kt
+++ app/src/main/java/com/example/rockpapersciccors/controllers/History.kt
@@ -2,12 +2,34 @@ package com.example.rockpapersciccors.controllers
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.DividerItemDecoration
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.example.rockpapersciccors.R
+import com.example.rockpapersciccors.models.Game
+import com.example.rockpapersciccors.models.GameAdapter
+import kotlinx.android.synthetic.main.activity_history.*
 
 class History : AppCompatActivity() {
+
+    private val games = arrayListOf<Game>()
+    private val gameAdapter = GameAdapter(games)
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_history)
+
+        init()
+    }
+
+    private fun init() {
+        rvGames.layoutManager = LinearLayoutManager(this@History, RecyclerView.VERTICAL, false)
+        rvGames.adapter = gameAdapter
+        rvGames.addItemDecoration(
+            DividerItemDecoration(
+                this@History,
+                DividerItemDecoration.VERTICAL
+            )
+        )
     }
 }
